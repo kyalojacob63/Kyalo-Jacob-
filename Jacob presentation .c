5bce449be99feb8a902da50be96160aafb92be98
kyalojacob63 <kyalojacob345@gmail.com>
2025-09-25 22:02:36 +0300

    Add files via upload

---
+++ Jacob presentation .c
@@ -0,0 +1,21 @@
+//simple C program (jacinta)
+/*
+Name: Jacob Kyalo Makau
+Reg:  CT100/G/26266/25
+Description:Jacob.program
+*/
+
+#include <stdio.h>//
+
+//main function
+int main (){
+  printf("Analogue electronics/n");
+  printf("Computer systems and organization/n");
+  printf("Fundamentals of ICT/n");
+  printf("Introduction to computer application/n");
+  printf("Essentials of accounting/n");
+  printf("Communication skills/n");
+  printf("Mathematics for science/n");
+  printf("Introduction to programming and Algorithms");
+  return 0;
+}
---
+++ kyalo jacob makau.c
@@ -0,0 +1,31 @@
+//simple C program(jacinta)
+/*
+Name:kyalo jacob makau
+Reg.no:CT100/G/26266/25
+
+Description:Jacinta program
+*/
+
+#include<stdio.h>//
+
+//main function
+int main(){
+
+int Height;
+printf("Enter Your Height");
+ scanf("%d",&Height);
+  printf("Your Height is %d",Height);
+
+int Weight;
+ printf(" Enter Your Weight");
+   scanf("%d",&Weight);
+  printf("Your Weight is %d",Weight );
+    
+ int contact;
+ printf(" Enter Your Contact");
+   scanf("%d",&contact);
+    
+printf("Your contact is %d",contact);
+return 0;
+} 
+
